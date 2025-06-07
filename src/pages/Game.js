73202c1e6953d70bf32ef2ab32203a8a573efe10
Tory <victorialeone9@gmail.com>
2025-06-06 20:47:00 -0400

    handleClick works, isDraw logic works, titleCard working fine

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,53 @@
+import '../styles/Game.css';
+import {useState} from "react";
+
+
+
 export default function Game() {
-    return <h1> I'm the game!</h1>
+
+    const [board, setBoard] = useState(Array(9).fill(''));
+    const [player, setPlayer] = useState(true); // Player 1 = true, Player 2 = false
+
+
+
+    function isWin({board}){
+        return false;
+    }
+
+    function isDraw({ board }) {
+        return board.every(el => el !== '') && !isWin({ board });
+    }
+
+    function titleCard({board, player}) {
+        const drawStatus = isDraw({ board });
+        if(drawStatus) { return <h1>It's A Draw! 🙈</h1>}
+        return <h1> {player ? 'Player 1' : 'Player 2'} </h1>;
+    }
+
+    function handleClick({i}){
+        if(board[i] !== '') return;
+        const newBoard = [...board];
+        newBoard[i] = player ? 'X' : 'O';
+        setBoard(newBoard);
+        setPlayer(!player);
+    }
+
+
+    function CreateGrid({ board }) {
+        return board.map((el, i) => (
+            <div key={i} onClick={() => handleClick({i})}>
+                {el}
+            </div>
+        ));
+    }
+
+
+    return (
+        <div className="Game">
+            {titleCard({board, player})}
+            <div className="container">
+               <CreateGrid board={board} player={player} />
+            </div>
+        </div>
+    )
 }
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import '../styles/Welcome.css';
+
+
 export default function NoPage() {
     return (
         <div>
-            <h2>404: Not Found</h2>
+            <h2>404: Page Not Found</h2>
         </div>
-    )
+    );
 }
+
